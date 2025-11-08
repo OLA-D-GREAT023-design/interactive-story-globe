91986381b50add3a1bb478e23bff0660f8a3dea4
OLA-D-GREAT023-design <opeyemimukaila30@gmail.com>
2025-11-08 22:53:00 +0100

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        sciFi: '#0ea5e9',
+        historical: '#9a7b4f',
+        cultural: '#6b21a8'
+      }
+    }
+  },
+  plugins: []
+};
